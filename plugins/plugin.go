fc47573f12864afe0fcfb34b129c8882ea075db6
kyokomi <kyoko1220adword@gmail.com>
2015-06-14 17:20:30 +0900

    stopを実装

--- plugins/plugin.go
+++ plugins/plugin.go
@@ -8,22 +8,42 @@ import (
 
 var (
 	ctx     context.Context
-	plugins = []plugin{}
+	plugins = []Plugin{}
+	stop    = false
 )
 
-type plugin struct {
+type Plugin struct {
 	Key interface{}
 	BotMessagePlugin
 }
 
+func (p Plugin) Name() string {
+	return fmt.Sprintf("%s", p.Key)
+}
+
 func AddPlugin(key interface{}, val BotMessagePlugin) {
 	fmt.Println("insert plugin ", key)
-	plugins = append(plugins, plugin{key, val})
+	plugins = append(plugins, Plugin{key, val})
 }
 
 func DelPlugin(key interface{}) {
 	// TODO: 未実装
 	panic("未実装")
+}
+
+// Stop bot stop
+func Stop() {
+	stop = true
+}
+
+// Start bot start
+func Start() {
+	stop = false
+}
+
+// GetPlugins returns all plugins
+func GetPlugins() []Plugin {
+	return plugins
 }
 
 func init() {
@@ -40,6 +60,10 @@ func Context() context.Context {
 }
 
 func ExecPlugins(ctx context.Context, message string) {
+	if stop {
+		return
+	}
+
 	for _, p := range plugins {
 		ok, m := p.CheckMessage(ctx, message)
 		if !ok {
