475b6e168b805d9c11a2d00aa38123571d533236
kyokomi <kyoko1220adword@gmail.com>
2015-08-23 20:57:53 +0900

    added circleci coverage

---
+++ circleci/test/post/gocoverage_all.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# 参考URL: http://www.songmu.jp/riji/entry/2015-01-15-goveralls-multi-package.html
+
+set -e
+
+# 中断した時とかにゴミが残らないようにcleanupする
+cleanup() {
+  retval=$?
+  if [ $tmpprof != "" ] && [ -f $tmpprof ]; then
+    rm -f $tmpprof
+  fi
+  exit $retval
+}
+trap cleanup INT QUIT TERM EXIT
+
+# メインの処理
+prof=${1:-".profile.cov"}
+echo "mode: count" > $prof
+gopath1=$(echo $GOPATH | cut -d: -f1)
+echo "mode: count" > $prof
+for pkg in $(go list ./...); do
+  tmpprof=$gopath1/src/$pkg/profile.tmp
+  go test -covermode=count -coverprofile=$tmpprof $pkg
+  if [ -f $tmpprof ]; then
+    cat $tmpprof | tail -n +2 >> $prof
+    rm $tmpprof
+  fi
+done
