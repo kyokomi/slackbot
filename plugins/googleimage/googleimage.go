ee67ecd22ef972297fcb6a005630737a9a62f5d1
kyokomi <kyoko1220adword@gmail.com>
2015-11-07 10:52:04 +0900

    apiリファクタ

--- plugins/googleimage/googleimage.go
+++ plugins/googleimage/googleimage.go
@@ -1,15 +1,11 @@
 package googleimage
 
 import (
-	"fmt"
 	"log"
 	"math/rand"
-	"net/http"
-	"net/url"
 	"strings"
 	"time"
 
-	"github.com/bitly/go-simplejson"
 	"github.com/kyokomi/slackbot/plugins"
 )
 
@@ -17,17 +13,15 @@ const endpointURL = "https://www.googleapis.com/customsearch/v1"
 
 type plugin struct {
 	rd     *rand.Rand
-	client *http.Client
+	client GoogleImageAPIClient
 	cx     string
 	apiKey string
 }
 
-func NewPlugin(cx string, apiKey string) plugins.BotMessagePlugin {
+func NewPlugin(client GoogleImageAPIClient) plugins.BotMessagePlugin {
 	return &plugin{
 		rd:     rand.New(rand.NewSource(time.Now().UnixNano())),
-		client: http.DefaultClient,
-		cx:     cx,
-		apiKey: apiKey,
+		client: client,
 	}
 }
 
@@ -38,35 +32,10 @@ func (p *plugin) CheckMessage(_ plugins.BotEvent, message string) (bool, string)
 func (p *plugin) DoAction(event plugins.BotEvent, message string) bool {
 	query := strings.Replace(strings.TrimLeft(message, "image me"), "image me", "", 1)
 
-	params := url.Values{}
-	params.Set("searchType", "image")
-	params.Set("alt", "json")
-	params.Set("cx", p.cx)
-	params.Set("key", p.apiKey)
-	params.Set("q", query)
-
-	resp, err := p.client.Get(fmt.Sprintf("%s?%s", endpointURL, params.Encode()))
+	links, err := p.client.GetImageLinks(query)
 	if err != nil {
 		log.Println(err)
 		return false
-	}
-
-	j, err := simplejson.NewFromReader(resp.Body)
-	if err != nil {
-		log.Println(err)
-		return false
-	}
-
-	items, err := j.Get("items").Array()
-	if err != nil {
-		log.Println(err)
-		return false
-	}
-
-	var links []string
-	for _, item := range items {
-		link := item.(map[string]interface{})["link"].(string)
-		links = append(links, link)
 	}
 
 	idx := int(p.rd.Int() % len(links))
--- plugins/googleimage/googleimage_test.go
+++ plugins/googleimage/googleimage_test.go
@@ -2,6 +2,7 @@ package googleimage_test
 
 import (
 	"log"
+	"net/http"
 	"testing"
 
 	"github.com/kyokomi/slackbot/plugins"
@@ -15,7 +16,7 @@ func init() {
 }
 
 func TestCheckMessage(t *testing.T) {
-	p := googleimage.NewPlugin("cx", "key")
+	p := googleimage.NewPlugin(googleimage.NewGoogleImageAPIClient(http.DefaultClient, "cx", "key"))
 	ok, _ := p.CheckMessage(testEvent, testEvent.BaseText())
 	if !ok {
 		t.Errorf("ERROR check = NG")
@@ -23,7 +24,7 @@ func TestCheckMessage(t *testing.T) {
 }
 
 func TestDoAction(t *testing.T) {
-	p := googleimage.NewPlugin("cx", "key")
+	p := googleimage.NewPlugin(googleimage.NewGoogleImageAPIClient(http.DefaultClient, "cx", "key"))
 
 	next := p.DoAction(testEvent, testEvent.BaseText())
 
