ff3911ffbccb6ed66b2ac552f1b58f55dc38be46
kyokomi <kyoko1220adword@gmail.com>
2016-12-25 17:22:45 +0900

    Add support RedisRepository

--- repository.go
+++ repository.go
@@ -1,30 +1,63 @@
 package slackbot
 
+import (
+	"gopkg.in/redis.v3"
+)
+
+// Repository is slackbot common repository interface
 type Repository interface {
 	Load(key string) (string, error)
 	Save(key string, value string) error
 	Close() error
 }
 
+// NewOnMemoryRepository create on memory repository
 func NewOnMemoryRepository() Repository {
-	return &OnMemoryRepository{
+	return &inMemoryRepository{
 		memory: map[string]string{},
 	}
 }
 
-type OnMemoryRepository struct {
+type inMemoryRepository struct {
 	memory map[string]string
 }
 
-func (r *OnMemoryRepository) Load(key string) (string, error) {
+func (r *inMemoryRepository) Load(key string) (string, error) {
 	return r.memory[key], nil
 }
 
-func (r *OnMemoryRepository) Save(key string, value string) error {
+func (r *inMemoryRepository) Save(key string, value string) error {
 	r.memory[key] = value
 	return nil
 }
 
-func (r *OnMemoryRepository) Close() error {
+func (r *inMemoryRepository) Close() error {
 	return nil
+}
+
+// NewRedisRepository create on redis repository
+func NewRedisRepository(addr, password string, db int64) Repository {
+	repo := &redisRepository{}
+	repo.redisDB = redis.NewClient(&redis.Options{
+		Addr:     addr,
+		Password: password,
+		DB:       db,
+	})
+	return repo
+}
+
+type redisRepository struct {
+	redisDB *redis.Client
+}
+
+func (r *redisRepository) Load(key string) (string, error) {
+	return r.redisDB.Get(key).Result()
+}
+
+func (r *redisRepository) Save(key string, value string) error {
+	return r.redisDB.Set(key, value).Err()
+}
+
+func (r *redisRepository) Close() error {
+	return r.redisDB.Close()
 }
